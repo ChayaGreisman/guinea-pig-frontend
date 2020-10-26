63caed0845679775b94daa47081e14cc436d8472
ChayaGreisman <chayamgreisman@gmail.com>
2020-10-25 21:12:24 -0400

    experimenting with canvas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Canvas from './components/Canvas'
+
 
 function App() {
+
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.fill()
+  }
+  
   return (
     <div className="App">
-      <img src = './guineaPigGIF.gif'/>
+      <br/>
+      <Canvas draw={draw}/>
+      <br/>
+      <img src = './guineaPigGIF.gif'/> 
     </div>
   );
 }
