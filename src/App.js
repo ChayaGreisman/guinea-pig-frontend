cbc745a3a51ccd101008cf1b9a5351eff15d52c1
ChayaGreisman <chayamgreisman@gmail.com>
2020-10-29 10:54:28 -0400

    just for fun

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Canvas from './components/Canvas'
+
 
 
 function App() {
 
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
-  
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <br/>
-      <Canvas draw={draw}/>
-      <br/>
-      <img src = './guineaPigGIF.gif'/> 
+      {[...Array(count)].map((e, i) => <img src='./guineaPigGIF.gif' onClick={()=>setCount(count+1)} /> )}
+      <img src='./guineaPigGIF.gif' onClick={()=>setCount(count+1)} /> 
     </div>
   );
 }
