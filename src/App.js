0fc65ae5db0d418b23232eb084e9244b21bd9b4d
ChayaGreisman <chayamgreisman@gmail.com>
2020-10-30 14:55:33 -0400

    semantic

--- src/App.js
+++ src/App.js
@@ -13,6 +13,53 @@ function App() {
       <br/>
       {[...Array(count)].map((e, i) => <img src='./guineaPigGIF.gif' onClick={()=>setCount(count+1)} /> )}
       <img src='./guineaPigGIF.gif' onClick={()=>setCount(count+1)} /> 
+
+
+      <div className="ui card room-card" >
+            <div className="content">
+                <div className="right floated meta">2 sec ago</div>
+                <img className="ui avatar image"  alt="user"/> @chaya
+            </div>
+            <div className="image">
+                <img className='room-image' />
+            </div>
+
+            <div className="extra content" style={{backgroundColor: 'lightGrey'}}>
+                <div className="center aligned">
+                    <button className="ui basic button ">
+                    <i className="eye icon"></i>
+                        View Room
+                    </button>
+                </div>
+            </div>
+            
+            <div className="content">
+                <h5><strong>hjvjg</strong></h5>
+                hgcxhfgxn
+            </div>
+            <div className="content">
+                <span className="right floated">
+                <i className=  "heart outline red like icon"></i>
+                2
+                </span>
+                <span >
+                    <i className="comment icon"></i>
+                    3 comments
+                </span>
+                <br/>
+                <hr/>
+               
+                
+            </div>
+            <div className="extra content">
+                <div className="ui large transparent left icon input">
+                <i className="heart outline icon"></i>
+                <input type="text"   placeholder="Add Comment..."/>
+                </div>
+            </div>
+        </div>
+
+
     </div>
   );
 }
